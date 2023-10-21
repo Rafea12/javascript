633448b210b410e2f49522ba752608673f1508a4
Rafea12 <142077896+Rafea12@users.noreply.github.com>
2023-10-21 01:35:17 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+//EXCERISE #3.1
+
+
+let snack = [ 'golgappy', 'chat' , 'momos', 'cake']
+ console.log (snack);
+ snack [3] = "macroni"
+ console.log(snack[3]);
+
+//EXCERIS#3.2
+
+let snack2 = [ 'golgappy', 'chat' , 'momos', 'cake']
+ console.log (snack2);
+ snack2 [3] = "macroni"
+ console.log(snack[3]);
+ snack2.pop ();
+ snack2.sort ();
+ let lastmacroni = snack2.lastIndexOf("macroni");
+ snack2 [0] = "pizza"
+ console.log(snack[1]);
